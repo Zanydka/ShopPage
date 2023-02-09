dfe32ae279c6af1606e901bbbc6a7dd66254d9d9
Denis <marvelick753@gmail.com>
2023-02-09 15:37:28 +0200

    add change currency

--- src/container/ShopPage/ShopPage.tsx
+++ src/container/ShopPage/ShopPage.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material'
 import ProductList from './ProductList'
 import './ShopPage.css'
 
@@ -7,12 +6,6 @@ type Props = {}
 const ShopPage = (props: Props) => {
     return (
         <>
-            <div className="center">
-                <Button variant="outlined">USD</Button>
-                <Button variant="outlined">EUR</Button>
-                <Button variant="outlined">UAH</Button>
-                <Button variant="outlined">PLN</Button>
-            </div>
             <ProductList />
         </>
     )
